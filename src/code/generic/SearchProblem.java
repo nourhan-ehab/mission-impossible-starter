a9b720e9c842d4a6a364e94d642f0c73014fb8df
nourhan-ehab <nourhan.ehab@guc.edu.eg>
2020-11-11 14:04:45 +0200

    Add files via upload

---
+++ src/code/generic/SearchProblem.java
@@ -0,0 +1,5 @@
+package code.generic;
+
+public abstract class SearchProblem {
+
+}
---
+++ src/code/mission/MissionImpossible.java
@@ -0,0 +1,12 @@
+package code.mission;
+
+import code.generic.SearchProblem;
+
+public class MissionImpossible extends SearchProblem {
+	
+	public static String solve (String grid, String strategy, boolean visualize) {
+		return null;
+	}
+	
+
+}
